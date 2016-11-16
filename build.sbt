2f0b05d6546105a6ec9d11412fe0ce272c2d43e6
Andrius Juodelis <juodelis@puzzle.ch>
2016-11-16 16:47:39 +0100

    init

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "prj2"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+unmanagedBase := baseDirectory.value / "lib"
---
+++ src/main/scala-2.11/Go.scala
@@ -0,0 +1,17 @@
+import ch.ethz.dal.tinyir.io.TipsterStream
+
+object Go extends App {
+
+  val tipster = new TipsterStream ("data")
+  println("Number of files in zips = " + tipster.length)
+
+  var length : Long = 0
+  var tokens : Long = 0
+  for (doc <- tipster.stream.take(10000)) {
+    length += doc.content.length
+    tokens += doc.tokens.length
+  }
+  println("Final number of characters = " + length)
+  println("Final number of tokens     = " + tokens)
+
+}
