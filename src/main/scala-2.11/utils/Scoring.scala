72bb05c9e208d2fef5e3eee7173ad40f1d39f5a2
Ralph Steurer <Ralph@Ralph-Steurer>
2016-11-27 19:13:57 +0100

    scoring done

--- src/main/scala-2.11/utils/Scoring.scala
+++ src/main/scala-2.11/utils/Scoring.scala
@@ -3,108 +3,112 @@ package utils
 import ch.ethz.dal.tinyir.processing.XMLDocument
 
 import scala.collection.Map
+import scala.collection.immutable.ListMap
 import scala.collection.mutable.ListBuffer
 
 /**
   * Created by Ralph on 20/11/16.
   */
 
-//todo, work in progress
 
 // query_results: [(Query ID, Rank), Doc ID]
-// Calculate metrics per Query. Except for MAP (which divides the APs of each query by the number of queries)
-class Scoring(val relevance_judgement: Map[(Int, Int, String), Int], val query_results: Map[(Int, Int), String]) {
+class Scoring(val relevance_judgement: Map[(Int, String), Int], val query_results: Map[(Int, Int), String]) {
+
+  val total_relevant_judged_docs: Map[(Int, String), Int] = relevance_judgement.filter((x) => x._2 == 1)
+  val queryIDs: List[Int] = query_results.map(result => result._1).map(key => key._1).toList.distinct
+  var metrics_all_queries = Map[Int, Array[Double]]()
+  var sum_ap_of_query: Double = 0.0
 
   // Returns the metrics per query. The List contains the metrics for each query
-  def calculateMetrics(): Map[Int, Array[Double]] = {
-
-
-    var metrics_all_queries = Map[Int, Array[Double]]()
-
-
-    val queryIDs: List[Int] = query_results.map(result => result._1).map(key => key._1).toList.distinct
-    println("query IDs " + queryIDs)
+  def calculateMetrics() {
 
     queryIDs.foreach{ queryID =>
 
-      // Calculate precision
-      println("all query results" + query_results)
-      println(query_results.map(x => x._1._1))
       val results_of_single_query = query_results.filter((key) => key._1._1 == queryID)
-      println("Results of single query " + results_of_single_query)
-      val precision_of_query = calculatePrecisionPerQuery(queryID, results_of_single_query)
-      println("Precision " + precision_of_query)
+
+      // Get all relevant docs for current query from the relevance judgement
+      val all_relevant_docs_for_query: List[String] = total_relevant_judged_docs.filter((x) => x._1._1 == queryID).map(x => x._1._2).toList
+
+      // Count how many documents returned by the query are correct based on the relevance judgement
+      var tp = 0.0
+      results_of_single_query.foreach(result_tuple => {
+        if (all_relevant_docs_for_query.contains(result_tuple._2)){
+          tp += 1
+        }
+      })
+
+      // Get TP + FP = all results of query
+      val tp_fp = results_of_single_query.size
+
+      // Get TP + FN = all relevant docs
+      val tp_fn = all_relevant_docs_for_query.length
+
+
+      // Calculate precision
+      val precision_of_query = calculatePrecisionPerQuery(tp, tp_fp)
+
 
       // Calculate recall
-
-      val recall_of_query = 1.0
+      val recall_of_query = calculateRecallPerQuery(tp, tp_fn)
 
       // Calculate F1
-
-      val f1_of_query = 1.0
+      val f1_of_query = calculateF1PerQuery(precision_of_query, recall_of_query)
 
       // Calculate AP
-
-
-      val ap_of_query = 1.0
+      val ap_of_query = calculateAvgPrecisionPerQuery(results_of_single_query, all_relevant_docs_for_query, tp_fn)
+      sum_ap_of_query += ap_of_query
 
       // Add results
       metrics_all_queries += (queryID -> Array(precision_of_query, recall_of_query, f1_of_query, ap_of_query))
 
     }
 
-    return metrics_all_queries
-
   }
 
-  private def calculatePrecisionPerQuery(query: Int, result: Map[(Int, Int), String]): Double = {
+  def getQueryMetrics(): Map[Int, Array[Double]] = { return metrics_all_queries }
 
-    // First, get all correct documents for the current query from the relevance judgement
-    // Filter on relevant doc (=1) and then filter for the current Query and then get only the Doc IDs (can be optimized :-) )
-    val total_relevant_docs: List[String] = relevance_judgement.filter((x) => x._2 == 1).filter((x) => x._1._1 == query).map(x => x._1._3).toList
-    println("total relevant docs " + total_relevant_docs)
+  def getMAP(): Double = { return sum_ap_of_query / queryIDs.length }
 
-    // Count how many documents returned by the query are correct based on the relevance judgement
-    var tp = 0.0
-    result.foreach(result_tuple => {
-      println("result tuple " + result_tuple._2)
-      if (total_relevant_docs.contains(result_tuple._2)){
-        tp += 1
+
+  private def calculatePrecisionPerQuery(tp: Double, tp_fp: Double): Double = {
+    if (tp_fp == 0){ return 0 }
+    return tp / tp_fp
+  }
+
+  private def calculateRecallPerQuery(tp: Double, tp_fn: Double): Double = {
+    if (tp_fn == 0){ return 0 }
+    return tp / tp_fn
+  }
+
+  private def calculateF1PerQuery(precision: Double, recall: Double): Double = {
+    if (precision + recall == 0) {return 0}
+    return 2 * ((precision * recall) / (precision + recall))
+  }
+
+  // The AP is calculated "raw" and using strategy 1 (whole corpus)
+  private def calculateAvgPrecisionPerQuery(query_result: Map[(Int, Int), String], rel_docs: List[String], tp_fn: Double): Double = {
+
+    var precision_at_rank_k: Double = 0
+    var total_precision: Double = 0
+    var correct_docs_at_rank_k: Double = 0 // number of correct docs returned by query at rank k
+    var returned_docs_at_rank_k: Double = 0 // total number docs returned by query at rank k
+
+    if (tp_fn == 0) { return 0.0 }
+
+    val query_result_sorted_by_rank = ListMap(query_result.toSeq.sortWith(_._1._2 < _._1._2):_*)
+
+    query_result_sorted_by_rank.foreach(result_tuple => {
+      returned_docs_at_rank_k += 1
+      if (rel_docs.contains(result_tuple._2)){
+        correct_docs_at_rank_k += 1
+        precision_at_rank_k = correct_docs_at_rank_k / returned_docs_at_rank_k
+        total_precision = total_precision + precision_at_rank_k
       }
     })
-    println("TP " + tp)
 
-    // Get TP + FP = all results of query
-    val tp_fp = result.size
-
-    // Now calculate it
-    return tp / tp_fp
+    return total_precision / tp_fn
 
   }
-
-
-  private def calculateRecallPerQuery(): Double = {
-
-    1.0 //dummy
-  }
-
-
-
-  private def calculateF1PerQuery(): Double = {
-
-    1.0
-  }
-
-  private def calculateAvgPrecisionPerQuery(): Double = {
-    1.0
-
-  }
-
-  private def calculateMAP(): Double = {
-    1.0
-
-  }
-
 
 
 }
--- src/main/scala-2.11/utils/ScoringUnitTest.scala
+++ src/main/scala-2.11/utils/ScoringUnitTest.scala
@@ -10,6 +10,7 @@ object ScoringUnitTest extends App {
   var relevance_judgement = Map[(Int, Int, String), Int]()
   var query_results = Map[(Int, Int), String]()
 
+  /*
   // add some dummy values
   relevance_judgement += ((1, 0, "DOC-A") -> 1)
   relevance_judgement += ((1, 0, "DOC-B") -> 1)
@@ -26,13 +27,54 @@ object ScoringUnitTest extends App {
 
   query_results += ((2,1) -> "DOC-A")
   query_results += ((2,2) -> "DOC-E")
+  */
 
-  //println(relevance_judgement)
-  //println(query_results)
+// To test AP as in the lecture slides
+  relevance_judgement += ((1, 0, "DOC-A") -> 1)
+  relevance_judgement += ((1, 0, "DOC-C") -> 1)
+  relevance_judgement += ((1, 0, "DOC-F") -> 1)
+  relevance_judgement += ((1, 0, "DOC-I") -> 1)
+  relevance_judgement += ((1, 0, "DOC-J") -> 1)
 
-  val myScoring = new Scoring(relevance_judgement, query_results)
+  // 10 results but only 5 are correct
+  query_results += ((1,1) -> "DOC-A")
+  query_results += ((1,2) -> "DOC-B")
+  query_results += ((1,3) -> "DOC-C")
+  query_results += ((1,4) -> "DOC-D")
+  query_results += ((1,5) -> "DOC-E")
+  query_results += ((1,6) -> "DOC-F")
+  query_results += ((1,7) -> "DOC-G")
+  query_results += ((1,8) -> "DOC-H")
+  query_results += ((1,9) -> "DOC-I")
+  query_results += ((1,10)-> "DOC-J")
 
-  val metrics = myScoring.calculateMetrics()
+  relevance_judgement += ((2, 0, "DOC-B") -> 1)
+  relevance_judgement += ((2, 0, "DOC-E") -> 1)
+  relevance_judgement += ((2, 0, "DOC-G") -> 1)
+
+  // 10 results but only 3 are correct
+  query_results += ((2,1) -> "DOC-A")
+  query_results += ((2,2) -> "DOC-B")
+  query_results += ((2,3) -> "DOC-C")
+  query_results += ((2,4) -> "DOC-D")
+  query_results += ((2,5) -> "DOC-E")
+  query_results += ((2,6) -> "DOC-F")
+  query_results += ((2,7) -> "DOC-G")
+  query_results += ((2,8) -> "DOC-H")
+  query_results += ((2,9) -> "DOC-I")
+  query_results += ((2,10)-> "DOC-J")
+
+
+
+  // Projection to project away the dummy number 0
+  val relevance_judgement_clean: Map[(Int, String), Int] = relevance_judgement.map(
+    rj_tuple => ((rj_tuple._1._1, rj_tuple._1._3), rj_tuple._2))
+
+  val myScoring = new Scoring(relevance_judgement_clean, query_results)
+  myScoring.calculateMetrics()
+
+  val metrics = myScoring.getQueryMetrics()
+  val meanAvgPrecision = myScoring.getMAP()
 
   /*
   metrics.foreach(metrics_per_query => {
@@ -46,5 +88,10 @@ object ScoringUnitTest extends App {
     println(" ")
   })
 
+  println("MAP is: " + meanAvgPrecision)
+
 
 }
+
+//map{
+//case (query, ignore, doc_name, relevance) => (query, ignore, doc_name, relevance) }
