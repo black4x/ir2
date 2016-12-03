d08e256e045067e5b817ca4c3b168271a6d0cdf9
Ralph Steurer <Ralph@Ralph-Steurer>
2016-12-03 15:51:50 +0100

    index sharding

---
+++ src/main/scala-2.11/Indexing/DocShard.scala
@@ -0,0 +1,39 @@
+package main
+
+import ch.ethz.dal.tinyir.io._
+import ch.ethz.dal.tinyir.processing._
+import ch.ethz.dal.tinyir.util.StopWatch
+import com.github.aztek.porterstemmer.PorterStemmer
+import scala.collection.mutable.ListBuffer
+
+class DocShard(var partstream:Stream[Document]) {
+    val myStopWatch = new StopWatch()
+    myStopWatch.start
+    /*creates the inverted index*/
+    val invertedTFIndex  =tfTuples.groupBy(_.term).mapValues(_.map(tfT => (tfT.doc, tfT.count)).toList.sorted)
+    myStopWatch.stop
+    println("Time elapsed to create invertedTFIndex:%s".format(myStopWatch.stopped))
+    
+    myStopWatch.start
+    val vocabulary=invertedTFIndex.keys
+    myStopWatch.stop
+    println("Time elapsed to create vocabulary:%s".format(myStopWatch.stopped))
+    
+    myStopWatch.start
+    val documentFrequency=invertedTFIndex.mapValues(list=>list.length) //Map from word (===token) to its document frequency
+    myStopWatch.stop
+    println("Time elapsed to create documentFrequency:%s".format(myStopWatch.stopped))
+   //    println(documentFrequency.toList.sortBy(-_._2))
+    
+    myStopWatch.start
+    //stores doc0->(doclength,#distinct words)
+    val documentLength = partstream.map(d =>(d.name,(tokenListFiltered(d.content).length,tokenListFiltered(d.content).distinct.length))).toMap //Map from doc to its length
+    myStopWatch.stop
+    println("Time elapsed to create documentLength:%s".format(myStopWatch.stopped)) 
+    val nDocs=documentLength.size
+        
+    def tfTuples ={partstream.flatMap(d =>tokenListFiltered(d.content).groupBy(identity).map{ case (tk,lst) => TfTuple(tk,d.name, lst.length)})}
+      
+    def tokenListFiltered(doccontent: String) = StopWords.filterOutSW(Tokenizer.tokenize(doccontent)).map(v=>PorterStemmer.stem(v))
+
+}
