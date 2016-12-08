a1c050643df3a53c09e4caaf203d4eade7c1827a
Ralph Steurer <Ralph@Ralph-Steurer>
2016-12-08 18:09:15 +0100

    fix doc ID duplicates

--- src/main/scala-2.11/Indexing/DocShard2.scala
+++ src/main/scala-2.11/Indexing/DocShard2.scala
@@ -3,50 +3,52 @@ package Indexing
 import ch.ethz.dal.tinyir.processing._
 import ch.ethz.dal.tinyir.util.StopWatch
 import com.github.aztek.porterstemmer.PorterStemmer
-import main.TfTuple
+import main.{MyTokenizer, TfTuple}
 
 import scala.collection.Map
 
-class DocShard2(var partstream:Stream[Document]) {
+class DocShard2(var partstream:Stream[Document],startindex:Int) {
 
     val myStopWatch = new StopWatch()
-
-    /*Map Doc Names to Integer Numbers*/
     myStopWatch.start
-    val docMap: Map[String, Int] = partstream.map(d => d.name).zip(Stream from 1).map(doc_tuple =>  doc_tuple._1 -> doc_tuple._2).toMap
+
+    //    val docMap: Map[String, Int] = partstream.map(d => d.name).zip(Stream from 1).map(doc_tuple =>  doc_tuple._1 -> doc_tuple._2).toMap
+    //    val documentLength = partstream.map(d =>(getDocID(d.name),(MyTokenizer.tokenListFiltered(d.content).length,MyTokenizer.tokenListFiltered(d.content).distinct.length))).toMap //Map from doc to its length
+
+    var docMap=Map[String, Int]()
+    var documentLength=Map[Int, (Int, Int)]()
+    var counter=startindex
+
+    partstream.foreach{ doc =>
+        docMap +=((doc.name,counter))
+        counter=counter+1
+        documentLength+=(getDocID(doc.name)->(MyTokenizer.tokenListFiltered(doc.content).length,MyTokenizer.tokenListFiltered(doc.content).distinct.length))
+    }
     myStopWatch.stop
-    println("docMap created " + myStopWatch.stopped)
-    //docMap foreach (doc => println(doc))
+    println("Time elapsed to create docMap and documentLength:%s documentLength.size:%d".format(myStopWatch.stopped,documentLength.size))
+    val nDocs=documentLength.size
 
-    myStopWatch.start
+
     /*creates the inverted index*/
-    val invertedTFIndex: Map[String, List[(Int, Int)]] = tfTuples.groupBy(_.term).mapValues(_.map(tfT => (tfT.doc, tfT.count)).toList.sorted)
+    val invertedTFIndex:Map[String,List[(Int,Int)]] = tfTuples.groupBy(_.term).mapValues(_.map(tfT => (tfT.doc, tfT.count)).toList.sorted)
     myStopWatch.stop
-    println("Time elapsed to create invertedTFIndex:%s".format(myStopWatch.stopped))
-    
+    println("Time elapsed to create invertedTFIndex:%s size:".format(myStopWatch.stopped))
+
     myStopWatch.start
     val vocabulary=invertedTFIndex.keys
     myStopWatch.stop
-    println("Time elapsed to create vocabulary:%s".format(myStopWatch.stopped))
-    
+    //    println("Time elapsed to create vocabulary:%s".format(myStopWatch.stopped))
+
     myStopWatch.start
     val documentFrequency=invertedTFIndex.mapValues(list=>list.length) //Map from word (===token) to its document frequency
     myStopWatch.stop
-    println("Time elapsed to create documentFrequency:%s".format(myStopWatch.stopped))
-   //    println(documentFrequency.toList.sortBy(-_._2))
-    
-    myStopWatch.start
-    //stores doc0->(doclength,#distinct words)
-    val documentLength: Map[Int, (Int, Int)] = partstream.map(d =>(getDocID(d.name),(tokenListFiltered(d.content).length,tokenListFiltered(d.content).distinct.length))).toMap //Map from doc to its length
-    myStopWatch.stop
-    println("Time elapsed to create documentLength:%s".format(myStopWatch.stopped)) 
-    val nDocs=documentLength.size
-        
-    def tfTuples ={partstream.flatMap(d =>tokenListFiltered(d.content).groupBy(identity).map{ case (tk,lst) => TfTuple2(tk,getDocID(d.name), lst.length)})}
-      
-    def tokenListFiltered(doccontent: String) = StopWords.filterOutSW(Tokenizer.tokenize(doccontent)).map(v=>PorterStemmer.stem(v))
+    //    println("Time elapsed to create documentFrequency:%s".format(myStopWatch.stopped))
+    //    println(documentFrequency.toList.sortBy(-_._2))
 
-    /* Get the interger number previously assigned to each Doc Name*/
+
+
+    def tfTuples =partstream.flatMap(d =>MyTokenizer.tokenListFiltered(d.content).groupBy(identity).map{ case (tk,lst) => TfTuple2(tk,getDocID(d.name), lst.length)})
+
     def getDocID(docName: String): Int = {
         // must always find an entry
         return docMap.getOrElse(docName,0)
--- src/main/scala-2.11/Indexing/QuerySystemWithSharding2.scala
+++ src/main/scala-2.11/Indexing/QuerySystemWithSharding2.scala
@@ -2,7 +2,7 @@ package Indexing
 
 import ch.ethz.dal.tinyir.processing._
 import com.github.aztek.porterstemmer.PorterStemmer
-import main.DocShard
+import main.MyTokenizer
 
 import scala.collection.mutable.ListBuffer
 
@@ -15,92 +15,98 @@ class QuerySystemWithSharding2(var wholestream:Stream[Document], chuncksize:Int 
   print("** Total Memory: " + runtime.totalMemory/1000000)
   println("** Max Memory:   " + runtime.maxMemory/1000000)
 
-    var count=0
-    var docShards = new ListBuffer[DocShard2]()
-    while ( ! wholestream.isEmpty){
-      var partstream=wholestream.take(chuncksize)
-      wholestream=wholestream.drop(chuncksize)
-      count = count + 1
-      println(count)
-      val docShard = new DocShard2(partstream)
-      docShards+=docShard
+  var docShards = new ListBuffer[DocShard2]()
+  var counter=1
+  while ( ! wholestream.isEmpty){
 
-      print("** Used Memory:  " + (runtime.totalMemory - runtime.freeMemory)/1000000)
-      print("** Free Memory:  " + runtime.freeMemory/1000000)
-      print("** Total Memory: " + runtime.totalMemory/1000000)
-      println("** Max Memory:   " + runtime.maxMemory/1000000)
-    }  
+    var partstream=wholestream.take(chuncksize)
+    wholestream=wholestream.drop(chuncksize)
+    val docShard= new DocShard2(partstream,counter)
+    docShards+=docShard
+    counter=counter+chuncksize
 
-  
-    def documentFrequency(token:String) = docShards.toList.map(x=>x.documentFrequency.getOrElse(token,0)).sum
-    
-    def documentLength(doc:Int)={
-      var loop = 0 
-      var notFound = true
-      var res=(0,0)
-      while(res==(0,0) && loop < docShards.length){
-         res = docShards(loop).documentLength.getOrElse(doc,(0,0))
-         loop=loop+1
-      }
-      res
-    }
-    
-    val nDocs=docShards.toList.map(x=>x.documentLength.size).sum 
-
-    def query(queryID: Int, querystring:String): Map[(Int, Int), String] = {
-      val tokenList= tokenListFiltered(querystring)
-      var candidateDocs=Seq[Int]()
-      for (loop <- 0 until docShards.length) {
-        val candidateDocsShard: Seq[Int] = tokenList.flatMap(token=>docShards(loop).invertedTFIndex.getOrElse(token,List())).map(pair=>pair._1).distinct
-        candidateDocs = candidateDocs.union(candidateDocsShard)
-      }
-      candidateDocs.map(candidateDoc=>(candidateDoc,scoring(tokenList,candidateDoc))).sortBy(-_._2).zip(Stream from 1).take(100)
-        .map(result_tuple => ((queryID, result_tuple._2), getDocName(result_tuple._1._1))).toMap
-    }
-    
-    
-    /*Input a eg content of doc, output a list of tokens without stopwords and stemmed*/
-    def tokenListFiltered(doccontent: String) = StopWords.filterOutSW(Tokenizer.tokenize(doccontent)).map(v=>PorterStemmer.stem(v))
-    
-    /*tfTuples returns for an input stream of documents the list of postings including termfrequencies*/
-    
-    
-    def log2(x:Double):Double= math.log(x)/math.log(2)
-    
-    def scoring(queryTokenList:Seq[String],doc:Int) = {
-      //val scorings=queryTokenList.map(token=>invertedTFIndexReturnTF(token,doc))
-      //val scorings=queryTokenList.map(token=>math.log(invertedTFIndexReturnTF(token,doc)+1.0)/(documentLength(doc)._1+documentLength(doc)._2))
-      
-      //the following function normalized the tf with the document length. Hence longer docs are not favourte. Used 
-      val scorings=queryTokenList.map(token=>log2((invertedTFIndexReturnTF(token,doc)+1.0)/(documentLength(doc)._1+documentLength(doc)._2))*
-                                             (log2(nDocs)-log2(documentFrequency(token))))
-                                      
-      val result=scorings.sum
-      result
-    }
-    
-    /*Given a document and a token, this function returns the term frequency. Works also if token is not in the doc => tf=0*/
-    def invertedTFIndexReturnTF(token:String,doc:Int)={
-      var loop =0
-      var res=List[(Int,Int)]()
-      
-      while(res.isEmpty && loop<docShards.length){
-         res = docShards(loop).invertedTFIndex.getOrElse(token,List()).filter(_._1 == doc)
-         loop=loop+1
-         
-      }
-      res.map(pair=>pair._2).sum
-    }
-
-    def getDocName(docID: Int): String ={
-      // can only be one match for submitted Doc ID
-      var filteredDoc: String = " "
-      var loop = 0
-      while(!filteredDoc.isEmpty && loop < docShards.length) {
-        filteredDoc = docShards(loop).docMap.filter(doc_tuple => (doc_tuple._2 == docID)).map(relevant_doc => relevant_doc._1).head
-        loop = loop + 1
-      }
-      return filteredDoc
+    print("** Used Memory:  " + (runtime.totalMemory - runtime.freeMemory)/1000000)
+    print("** Free Memory:  " + runtime.freeMemory/1000000)
+    print("** Total Memory: " + runtime.totalMemory/1000000)
+    println("** Max Memory:   " + runtime.maxMemory/1000000)
   }
-    
+
+  def getDocName(docID: Int): String ={
+    // can only be one match for submitted Doc ID
+
+    var loop = 0
+    var filteredDoc=List[String]()
+    while(loop < docShards.length) {
+      val filteredDoc = docShards(loop).docMap.filter(doc_tuple => (doc_tuple._2 == docID)).map(relevant_doc => relevant_doc._1).toList
+      if(filteredDoc.length>0){
+        return filteredDoc.head
+      }
+      loop = loop + 1
+    }
+    return filteredDoc.head
+  }
+
+
+  def documentFrequency(token:String)=docShards.toList.map(x=>x.documentFrequency.getOrElse(token,0)).sum
+
+  def documentLength(doc:Int)={
+    var loop = 0
+    var notFound = true
+    var res=(0,0)
+    while(res==(0,0) && loop<docShards.length){
+      res=docShards(loop ).documentLength.getOrElse(doc,(0,0))
+      loop=loop+1
+    }
+    res
+  }
+
+  val nDocs=docShards.toList.map(x=>x.documentLength.size).sum
+
+  def query(queryID:Int,querystring:String): Map[(Int, Int), String]={
+    val tokenList= MyTokenizer.tokenListFiltered(querystring)
+    var candidateDocs=Seq[Int]()
+    for (loop <- 0 until docShards.length) {
+      val candidateDocsShard=tokenList.flatMap(token=>docShards(loop).invertedTFIndex.getOrElse(token,List())).map(pair=>pair._1).distinct
+      candidateDocs=candidateDocs.union(candidateDocsShard)
+    }
+    println(candidateDocs.map(candidateDoc=>(candidateDoc,scoring(tokenList,candidateDoc))).sortBy(-_._2))
+    val res=candidateDocs
+    val res1=candidateDocs.map(candidateDoc=>(candidateDoc,scoring(tokenList,candidateDoc))).sortBy(-_._2).zip(Stream from 1).take(100)
+    val res2=res1.map(result_tuple => ((queryID, result_tuple._2), getDocName(result_tuple._1._1))).toMap
+    res2
+
+  }
+  //$ is special sign in regular expression
+
+
+  /*tfTuples returns for an input stream of documents the list of postings including termfrequencies*/
+
+
+  def log2(x:Double):Double= math.log(x)/math.log(2)
+
+  def scoring(queryTokenList:Seq[String],doc:Int)={
+    //val scorings=queryTokenList.map(token=>invertedTFIndexReturnTF(token,doc))
+    //val scorings=queryTokenList.map(token=>math.log(invertedTFIndexReturnTF(token,doc)+1.0)/(documentLength(doc)._1+documentLength(doc)._2))
+
+    //the following function normalized the tf with the document length. Hence longer docs are not favourte. Used
+    val scorings=queryTokenList.map(token=>log2((invertedTFIndexReturnTF(token,doc)+1.0)/(documentLength(doc)._1+documentLength(doc)._2))*
+      (log2(nDocs)-log2(documentFrequency(token))))
+
+    val result=scorings.sum
+    result
+  }
+
+  /*Given a document and a token, this function returns the term frequency. Works also if token is not in the doc => tf=0*/
+  def invertedTFIndexReturnTF(token:String,doc:Int)={
+    var loop =0
+    var res=List[(Int,Int)]()
+
+    while(res.isEmpty && loop<docShards.length){
+      // res=docShards(loop).invertedTFIndex.getOrElse(token,List())
+      res=docShards(loop).invertedTFIndex.getOrElse(token,List()).filter(_._1==doc)
+      loop=loop+1
+
+    }
+    res.map(pair=>pair._2).sum
+  }
 }
